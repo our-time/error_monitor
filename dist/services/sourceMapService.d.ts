c1d37eaa4fb776e5c5917bc3c778030e2ad9cebb
ZiXiu <zixiu@aratum.cn>
2025-04-05 21:08:15 +0800

    remove node_modules and dist

--- dist/services/sourceMapService.d.ts
+++
@@ -1,9 +0,0 @@
-import { SourceMapConfig } from '../types/config';
-export declare class SourceMapService {
-    private config;
-    constructor(config: SourceMapConfig);
-    isEnabled(): boolean;
-    mapStackTrace(stack: string): Promise<string>;
-    uploadSourceMap(sourceMapFile: File, sourceFile: string): Promise<boolean>;
-    private readFileAsText;
-}
--- dist/utils/errorUtils.d.ts
+++
@@ -1,8 +0,0 @@
-interface ErrorInfo {
-    message: string;
-    name: string;
-    stack: string;
-}
-export declare function getErrorInfo(error: Error | any): ErrorInfo;
-export declare function generateErrorId(errorInfo: ErrorInfo): string;
-export {};
